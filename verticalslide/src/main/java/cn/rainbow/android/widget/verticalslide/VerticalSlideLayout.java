64f1115f0b8281f4b90a9eb42da3c6a56f49ff4a
bvin <bvin@live.cn>
2017-09-07 18:30:32 +0800

    add library

---
+++ verticalslide/src/main/java/cn/rainbow/android/widget/verticalslide/VerticalSlideLayout.java
@@ -0,0 +1,24 @@
+package cn.rainbow.android.widget.verticalslide;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by bvin on 2017/9/7.
+ */
+
+public class VerticalSlideLayout extends ViewGroup {
+    public VerticalSlideLayout(Context context) {
+        super(context);
+    }
+
+    public VerticalSlideLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
