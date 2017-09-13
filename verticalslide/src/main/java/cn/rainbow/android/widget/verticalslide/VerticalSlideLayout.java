3c8c6b1ee9f84b5ce74c44357ebeae18f33feb8e
bvin <bvin@live.cn>
2017-09-13 19:28:05 +0800

    移动

--- verticalslide/src/main/java/cn/rainbow/android/widget/verticalslide/VerticalSlideLayout.java
+++ verticalslide/src/main/java/cn/rainbow/android/widget/verticalslide/VerticalSlideLayout.java
@@ -2,7 +2,10 @@ package cn.rainbow.android.widget.verticalslide;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 
 /**
@@ -10,12 +13,21 @@ import android.view.ViewGroup;
  */
 
 public class VerticalSlideLayout extends ViewGroup {
+
+    private static final String TAG = "VerticalSlideLayout";
+
+    private final int mTouchSlop;
+    private float mInitialDownY;
+    private float mInitialMotionY;
+    private float mMovedOffset;
+
     public VerticalSlideLayout(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public VerticalSlideLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
     }
 
     @Override
@@ -27,11 +39,47 @@ public class VerticalSlideLayout extends ViewGroup {
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         int childCount = getChildCount();
-        int childTop = t;
+        int childTop = t + (int) mMovedOffset;
         for (int i = 0; i < childCount; i++) {
             View child = getChildAt(i);
             child.layout(l, childTop, r, b);
             childTop += child.getMeasuredHeight();
         }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        Log.d(TAG, "onInterceptTouchEvent: " + ev.getAction());
+        boolean mIsBeingDragged = super.onInterceptTouchEvent(ev);
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mInitialDownY = ev.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float currentY = ev.getY();
+                if (currentY - mInitialDownY > mTouchSlop) {
+                    mInitialMotionY = mInitialDownY + mTouchSlop;
+                    Log.d(TAG, "onInterceptTouchEvent: 满足事件拦截条件");
+                    mIsBeingDragged = true;
+                }
+                break;
+        }
+        return mIsBeingDragged;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, "onTouchEvent: " + event.getAction());
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                final float y = event.getY();
+                mMovedOffset = y - mInitialMotionY;
+                Log.d(TAG, "onTouchEvent: 移动距离 " + mMovedOffset);
+                requestLayout();
+                break;
+        }
+        return true;
     }
 }
